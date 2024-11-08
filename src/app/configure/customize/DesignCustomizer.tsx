588a4a2a35cfbc3c6a2ae43db04610fdd9ac3be8
saqibtanveer786 <elven5055@gmail.com>
2024-11-08 02:10:35 -0500

    made customize design section look good

--- src/app/configure/customize/DesignCustomizer.tsx
+++ src/app/configure/customize/DesignCustomizer.tsx
@@ -6,20 +6,19 @@ import { ArrowRight, Check } from 'lucide-react'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import Confetti from 'react-dom-confetti'
-import DesginCustomizeForm from './DesginCustomizeForm'
 import { TextElement, textElements } from './dummyTextElements'
 import * as fabric from 'fabric';
 import { FabricImage } from 'fabric'; // Make sure to import FabricImage
 
 function DesignCustomizer() {
     const [textElementState, setTextElementState] = useState<TextElement[]>(textElements)
-    // let canvas: fabric.Canvas;
+    let canvas: fabric.Canvas;
     useEffect(()=> {
       const img = makeImgEle()
       const width = img.width
       const height = img.height
 
-      const canvas = new fabric.Canvas("myCanvas", {width, height})
+      canvas = new fabric.Canvas("myCanvas", {width, height})
 
       const scale = calScale(canvas, img)
       
@@ -37,9 +36,7 @@ function DesignCustomizer() {
         left: ele.left,
         fontFamily: ele.fontFamily,  
         fill: ele.fontColor,
-        fontSize: 23,
-        height: 30,
-        width: 30
+        fontSize: ele.fontSize,
       })
 
       canvas.add(text)
@@ -75,23 +72,36 @@ function DesignCustomizer() {
       return scale
     }
 
+    const exportImage = ()=> {
+        const binary = canvas.toDataURL({format: 'png', multiplier: 4})
+
+
+        // Create a link to download the image
+        const link = document.createElement('a');
+        link.href = binary;          // Set the data URL as the href for the link
+        link.download = 'design.png'; // Set the default filename
+        link.click(); 
+    }
+
     return (
         <>
-          <div className='mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
+          <div className='flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
                 
             <div className='mt-6 sm:col-span-12 md:row-end-1 border'>
+              <div className='flex justify-between p-4'>
               <h3 className='text-3xl font-bold tracking-tight text-gray-900'>
                 Personalize Your Design
               </h3>
+                <Button onClick={exportImage}>Next</Button>
+              </div>
     
             <div className='sm:col-span-12 md:col-span-12 text-base border'>
-              <div className='grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-3 sm:items-center sm:gap-x-6 sm:py-6 md:py-10'>
-                <div className='flex items-center justify-center col-span-2'>
+              <div className='grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:items-center sm:gap-x-6 sm:py-6 md:py-10'>
+                <div className='flex items-center justify-center'>
                       <canvas id='myCanvas' className='absolute'></canvas>
                 </div>
-                <div className='col-span-1'>
-                    {/* <DesginCustomizeForm textElements={textElementState} setTextElements={setTextElementState}/> */}
-                </div>
+                {/* <div className='col-span-1'>
+                </div> */}
               </div>
             </div>
             </div>
--- src/app/configure/customize/dummyTextElements.ts
+++ src/app/configure/customize/dummyTextElements.ts
@@ -4,54 +4,60 @@ export const textElements: TextElement[] = [
         title: "top",
         content: "Save The D",
         top: 100,
-        left: 150,
+        left: 130,
         fontFamily: "Cursive",
-        fontColor: "#825700"
+        fontColor: "#825700",
+        fontSize: 23
     },
     {
         id: '1',
         title: "name",
         content: "Isable Morcade",
-        top: 100,
-        left: 150,
+        top: 130,
+        left: 100,
         fontFamily: "Cursive",
-        fontColor: "#825700"
+        fontColor: "#825700",
+        fontSize: 33
     },
     {
         id: '2',
         title: "and",
         content: "&",
-        top: 150,
+        top: 160,
         left: 150,
         fontFamily: "Cursive",
-        fontColor: "red"
+        fontColor: "red",
+        fontSize: 23
     },
     {
         id: '3',
         title: "second name",
         content: "Chad Gibbons",
-        top: 200,
-        left: 150,
+        top: 190,
+        left: 100,
         fontFamily: "Cursive",
-        fontColor: "#825700"
+        fontColor: "#825700",
+        fontSize: 33
     },
     {
         id: '4',
         title: "time",
         content: "two Oclick in the afternoon",
-        top: 200,
-        left: 150,
+        top: 300,
+        left: 80,
         fontFamily: "Cursive",
-        fontColor: "#825700"
+        fontColor: "#825700",
+        fontSize: 23
     },
     {
         id: '5',
         title: "reception",
         content: "Reception to fellow",
-        top: 200,
-        left: 150,
+        top: 330,
+        left: 100,
         fontFamily: "Cursive",
-        fontColor: "#825700"
+        fontColor: "#825700",
+        fontSize: 23
     },
 ]
 
@@ -63,4 +69,5 @@ export type TextElement = {
     left: number,
     fontFamily: string,
     fontColor: string,
+    fontSize: number,
 }
